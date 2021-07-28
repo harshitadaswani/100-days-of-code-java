eb858401fb477accb84c1eb9ec507329ff219ace
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-07-29 00:36:45 +0530

    declaring var and java operators lesson 5

--- helloWorld.java
+++ helloWorld.java
@@ -2,5 +2,9 @@ public class helloWorld {
 
   public static void main(String[] args)  {
     System.out.println("Hello world");
+    int n=5;
+    int m=5+6;
+    System.out.println(n);
+    System.out.println(m);
   }
 }
