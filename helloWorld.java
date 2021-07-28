21f4dc14a54e806768b1f47020036ce161bf4c8d
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-07-29 00:39:53 +0530

    using expressions lesson 6

--- helloWorld.java
+++ helloWorld.java
@@ -4,7 +4,9 @@ public class helloWorld {
     System.out.println("Hello world");
     int n=5;
     int m=5+6;
+    int t = m+n;
     System.out.println(n);
     System.out.println(m);
+    System.out.println(t);
   }
 }
