f46935d5b4e181858612d3c456caf3ef86e3b6f5
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-07-29 01:22:44 +0530

    primitive data type int byte

--- helloWorld.java
+++ helloWorld.java
@@ -8,5 +8,19 @@ public class helloWorld {
     System.out.println(n);
     System.out.println(m);
     System.out.println(t);
+
+    int min = Integer.MIN_VALUE;
+    int max = Integer.MAX_VALUE;
+    System.out.println("Minimum Integer Value:"+min);
+    System.out.println("Maximum Integer Value:"+max);
+    System.out.println("Busted Integer Value:"+(min-1));
+    System.out.println("Busted Integer Value:"+(max+1));
+
+    byte min = Byte.MIN_VALUE;
+    byte max = Byte.MAX_VALUE;
+    System.out.println("Minimum Integer Value:"+min);
+    System.out.println("Maximum Integer Value:"+max);
+    System.out.println("Busted Integer Value:"+(min-1));
+    System.out.println("Busted Integer Value:"+(max+1));
   }
 }
