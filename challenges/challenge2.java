21dd05f89c81e8dbfece2627dcbc2553c12b0173
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-03 00:12:30 +0530

    challenge2 final commit

--- challenges/challenge2.java
+++ challenges/challenge2.java
@@ -1,18 +1,25 @@
 public class challenge2 {
   public static void main(String[] args)  {
-
+    double number = 10.5;
+    printConversion(number);
   }
 
-  public static void toMilesPerHour(double kmh) {
-
+  public static int toMilesPerHour(double kmh) {
+  if(kmh < 0){
+    return -1;
+  }
+  else  {
+    return (int) Math.round(kmh/1.609);
+  }
   }
 
   public static void printConversion(double kmh)  {
-    if(kmh>0) {
-      System.out.println(kmh+"=km/h"+mile+"=mi/h");
+    if(kmh<0) {
+      System.out.println("Invalid value");
     }
     else  {
-      System.out.println("Invalid value");
+      long mile = toMilesPerHour(kmh);
+      System.out.println(kmh+" km/h = "+mile+" mi/h");
     }
   }
 }
