86a65fa5edc31c706725ba26251b4e9ebf479ab6
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-03 01:03:09 +0530

    leapYear

---
+++ challenges/leapyear.java
@@ -0,0 +1,20 @@
+public class leapyear {
+  public static void main(String[] args){
+  int num = 2002;
+  isLeapYear(num);
+  }
+
+  public static void isLeapYear(int year)  {
+    if(year >= 0 && year <=9999)  {
+      if( (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0) )  {
+        System.out.println(year+" is a leap year");
+      }
+      else  {
+        System.out.println(year+" is not a leap year");
+      }
+    }
+    else  {
+      System.out.println("Enter a valid year");
+    }
+  }
+}
