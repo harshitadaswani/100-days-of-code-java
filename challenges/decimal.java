8993534628ae1895884fc0c1102be61862752beb
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-03 02:58:01 +0530

    error

---
+++ challenges/decimal.java
@@ -0,0 +1,66 @@
+public class decimal  {
+  
+  public static void main(String[] args)  {
+    boolean areEqualByThreeDecimalPlaces(1.234, 5.6789);
+  }
+
+  public static boolean areEqualByThreeDecimalPlaces(double n1, double n2)  {
+    int a = (int) one * 1000;
+        int b = (int) two * 1000;
+
+        if(a == b){
+            System.out.println(true);
+            return true;
+        }
+        else
+
+            System.out.println(false);
+        return false;
+    }
+  }
+}
+
+/* public class decimal  {
+  
+  public static void main(String[] args)  {
+    areEqualByThreeDecimalPlaces(3.175, 3.176);
+  }
+
+  public static boolean areEqualByThreeDecimalPlaces(double one, double two)  {
+    int a = (int) one * 1000;
+    int b = (int) two * 1000;
+
+    if(a == b){
+        System.out.println(true);
+        return true;
+    }
+    
+    else{
+        System.out.println(false);
+        return false;
+    }
+  }
+}
+
+public class DecimalConverter {
+    public static void main(String[] args) {
+
+        areEqualByThreeDecimalPlaces(-2.1756, -2.175);
+        
+    }
+
+
+    public static boolean areEqualByThreeDecimalPlaces(double one, double two) {
+        int a = (int) (one * 1000);
+        int b = (int) (two * 1000);
+        if(a==b) {
+            System.out.println("True");
+            return true;
+        } else {
+            System.out.println("False");
+            return false;
+        }
+        
+    }
+}
+*/
