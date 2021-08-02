cfd2b8198a0893353747860a300b153001ef9310
harshitadaswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-02 20:29:13 +0530

    initial commit

---
+++ challenge2.java
@@ -0,0 +1,18 @@
+public class challenge2 {
+  public static void main(String[] args)  {
+
+  }
+
+  public static void toMilesPerHour(double kmh) {
+
+  }
+
+  public static void printConversion(double kmh)  {
+    if(kmh>0) {
+      System.out.println(kmh+"=km/h"+mile+"=mi/h");
+    }
+    else  {
+      System.out.println("Invalid value");
+    }
+  }
+}
