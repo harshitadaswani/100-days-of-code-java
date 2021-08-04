774d2ea14559321018cb2199343c457dac5d76e2
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-05 03:14:56 +0530

    bonus time challenge

--- challenges/time.java
+++ challenges/time.java
@@ -1,6 +1,7 @@
 public class time {
   public static void main(String[] args) {
     System.out.println(getDurationString(65,6));
+    System.out.println(getDurationString(65));
   }
 
   public static String getDurationString(int minutes, int seconds) {
@@ -11,6 +12,19 @@ public class time {
       int hour = (minutes/60);
       int minutesLeft = (minutes%60);
       return "Hours: "+hour+" Minutes: "+minutesLeft+" Seconds: "+seconds;
+    }
+  }
+
+   public static String getDurationString(int seconds) {
+    if(seconds<0)  {
+      return "Invalid value";
+    }
+    else {
+      int minutes = (seconds/60);
+      int secondsLeft = (seconds%60);
+      String a = getDurationString(minutes,secondsLeft);
+      return a; 
+      //return Minutes: "+minutes+" Seconds: "+secondsLeft;
     }
   }
 }
