97b11b11f4e91ecacbeab6a8098b9d43df127700
Harshita Daswani <87665916+harshitadaswani@users.noreply.github.com>
2021-08-05 04:58:36 +0530

    bonus prime challenge

--- challenges/prime.java
+++ challenges/prime.java
@@ -1,17 +1,29 @@
 public class prime {
   public static void main(String[] args) {
     isPrime(5);
+
+    int count=0;
+    int i;
+    for(i=10; i<=50; i++){
+      if(isPrime(i)){
+        count++;
+      }
+      if(count==3){
+        System.out.println("Exiting the loop");
+        break;
+      }
+    }
   }
 
   private static boolean isPrime(int n) {
-    int i;
+    int j;
     if(n==1) {
       System.out.println("1 is neither odd or even");
       return false;
     }
 
-    for(i=2; i<=(n/2); i++) {
-      if(n%i == 0) {
+    for(j=2; j<=(n/2); j++) {
+      if(n%j == 0) {
         System.out.println(n+" is not a prime number");
         return false;
       }
